24d41bcb995139bbe40feaa73a0a949fb633efb8
Samuel Parke <samueljparke@gmail.com>
2025-06-01 21:16:43 +0100

    soldiity formatting

--- contracts/test/SupplyChain.t.sol
+++ contracts/test/SupplyChain.t.sol
@@ -21,12 +21,7 @@ contract SupplyChainTest is Test {
         uint256 price = 1 ether;
 
         vm.startPrank(sender);
-        supplyChain.createShipment{value: 1 ether}(
-            receiver,
-            pickupTime,
-            distance,
-            price
-        );
+        supplyChain.createShipment{value: 1 ether}(receiver, pickupTime, distance, price);
 
         vm.stopPrank();
 
@@ -58,12 +53,7 @@ contract SupplyChainTest is Test {
 
         vm.startPrank(sender);
         vm.expectRevert();
-        supplyChain.createShipment{value: 0.5 ether}(
-            receiver,
-            pickupTime,
-            distance,
-            price
-        );
+        supplyChain.createShipment{value: 0.5 ether}(receiver, pickupTime, distance, price);
         vm.stopPrank();
     }
 
@@ -73,23 +63,14 @@ contract SupplyChainTest is Test {
         uint256 price = 1 ether;
 
         vm.startPrank(sender);
-        supplyChain.createShipment{value: 1 ether}(
-            receiver,
-            pickupTime,
-            distance,
-            price
-        );
+        supplyChain.createShipment{value: 1 ether}(receiver, pickupTime, distance, price);
 
         supplyChain.startShipment(sender, receiver, 0);
         vm.stopPrank();
 
-        (, , , , , , SupplyChain.ShipmentStatus _status, ) = supplyChain
-            .getShipment(sender, 0);
+        (,,,,,, SupplyChain.ShipmentStatus _status,) = supplyChain.getShipment(sender, 0);
 
-        assertEq(
-            uint256(_status),
-            uint256(SupplyChain.ShipmentStatus.IN_TRANSIT)
-        );
+        assertEq(uint256(_status), uint256(SupplyChain.ShipmentStatus.IN_TRANSIT));
     }
 
     function testCreateAndAttemptToStartShipmentButWrongReceiver() public {
@@ -98,12 +79,7 @@ contract SupplyChainTest is Test {
         uint256 price = 1 ether;
 
         vm.startPrank(sender);
-        supplyChain.createShipment{value: price}(
-            receiver,
-            pickupTime,
-            distance,
-            price
-        );
+        supplyChain.createShipment{value: price}(receiver, pickupTime, distance, price);
 
         vm.expectRevert();
         supplyChain.startShipment(sender, sender, 0);
@@ -116,12 +92,7 @@ contract SupplyChainTest is Test {
         uint256 price = 1 ether;
 
         vm.startPrank(sender);
-        supplyChain.createShipment{value: price}(
-            receiver,
-            pickupTime,
-            distance,
-            price
-        );
+        supplyChain.createShipment{value: price}(receiver, pickupTime, distance, price);
 
         supplyChain.startShipment(sender, receiver, 0);
         supplyChain.completeShipment(sender, receiver, 0);
@@ -136,49 +107,25 @@ contract SupplyChainTest is Test {
         uint256 price = 1 ether;
 
         vm.startPrank(sender);
-        supplyChain.createShipment{value: 1 ether}(
-            receiver,
-            pickupTime,
-            distance,
-            price
-        );
+        supplyChain.createShipment{value: 1 ether}(receiver, pickupTime, distance, price);
 
         supplyChain.startShipment(sender, receiver, 0);
         supplyChain.completeShipment(sender, receiver, 0);
         vm.stopPrank();
 
-        (
-            ,
-            ,
-            ,
-            ,
-            ,
-            ,
-            SupplyChain.ShipmentStatus _status,
-            bool _isPaid
-        ) = supplyChain.getShipment(sender, 0);
+        (,,,,,, SupplyChain.ShipmentStatus _status, bool _isPaid) = supplyChain.getShipment(sender, 0);
 
         assertEq(_isPaid, true);
-        assertEq(
-            uint256(_status),
-            uint256(SupplyChain.ShipmentStatus.DELIVERED)
-        );
+        assertEq(uint256(_status), uint256(SupplyChain.ShipmentStatus.DELIVERED));
     }
 
-    function testCreateAndStartAndAttemptCompleteShipmentButWrongReceiver()
-        public
-    {
+    function testCreateAndStartAndAttemptCompleteShipmentButWrongReceiver() public {
         uint256 pickupTime = 12345;
         uint256 distance = 10;
         uint256 price = 1 ether;
 
         vm.startPrank(sender);
-        supplyChain.createShipment{value: price}(
-            receiver,
-            pickupTime,
-            distance,
-            price
-        );
+        supplyChain.createShipment{value: price}(receiver, pickupTime, distance, price);
 
         vm.expectRevert();
         supplyChain.startShipment(sender, sender, 0);
@@ -193,21 +140,11 @@ contract SupplyChainTest is Test {
         uint256 price = 1 ether;
 
         vm.startPrank(sender);
-        supplyChain.createShipment{value: 1 ether}(
-            receiver,
-            pickupTime,
-            distance,
-            price
-        );
+        supplyChain.createShipment{value: 1 ether}(receiver, pickupTime, distance, price);
         vm.stopPrank();
 
         vm.startPrank(sender);
-        supplyChain.createShipment{value: 10 ether}(
-            receiver,
-            pickupTime,
-            distance,
-            10 ether
-        );
+        supplyChain.createShipment{value: 10 ether}(receiver, pickupTime, distance, 10 ether);
         vm.stopPrank();
 
         assertEq(2, supplyChain.getAllTransactions().length);
