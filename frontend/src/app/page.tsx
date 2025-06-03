2181693cba02553ce05c123369ebab4bdbfd1057
Samuel Parke <samueljparke@gmail.com>
2025-06-03 11:42:39 +0100

    clean home hero feature. Hash, block and blockchain graphic and info added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 
+import { HomeHeroSection } from "@/components/home/HomeHeroSection";
 import WholeBlockchainDemo from "@/components/home/WholeBlockchainDemo";
 import {useAccount} from "wagmi"
 
 export default function Home() {
   return (
-    <main className="min-h-screen p-8">
+    <main className="min-h-screen">
+    <HomeHeroSection/>
     <WholeBlockchainDemo/>
   </main>
   );
--- frontend/src/components/home/HomeHeroSection.tsx
+++ frontend/src/components/home/HomeHeroSection.tsx
@@ -1,70 +1,100 @@
 "use client"
 
-// components/HeroSection.tsx
 import React from "react";
+import Image from "next/image";
+import hash from "../../../public/images/hash.png"
+import block from "../../../public/images/block.png"
+import blockchain from "../../../public/images/blockchain.png"
 
-export const HomeHeroSection: React.FC = () => {
-  return (
-    <section className="bg-[url('/images/supplyChain.png')] bg-black/50 bg-blend-overlay bg-cover bg-center py-20">
-      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16">
-        <div className="flex flex-col justify-center">
-          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 
-                         md:text-5xl lg:text-6xl dark:text-white">
-            Supply Chain
-          </h1>
-          <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
-            [supply chain]
+export const HomeHeroSection: React.FC = () => (
+  <section className="bg-white pt-20">
+    <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-20">
+      <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8">
+
+        <h1 className="text-gray-900 dark:text-white text-3xl md:text-5xl font-extrabold mb-2">
+          Blockchain
+        </h1>
+        <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-6">
+          Static websites are now used to bootstrap lots of websites and are becoming the basis for a
+          variety of tools that even influence both web designers and developers.
+        </p>
+      </div>
+
+  
+      <div className="grid md:grid-cols-3 gap-8">
+      {/* card 1 */}
+        <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-xl p-4 md:p-6">
+          <a
+            href="#"
+            className="bg-green-100 text-green-800 text-xl font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-green-400 mb-4"
+          >
+            1.
+          </a>
+          <h2 className="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">
+            What is a Hash?
+          </h2>
+          <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-6">
+            Learn how hashing creates unique digital fingerprint to prove that data hasn't changed.
           </p>
-          <div className="flex flex-col space-y-4 sm:flex-row sm:space-y-0">
-            <a
-              href="#"
-              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium 
-                         text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 
-                         focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
-            >
-              Get started
-              <svg
-                className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </a>
-            <a
-              href="#"
-              className="py-3 px-5 sm:ms-4 text-sm font-medium text-gray-900 focus:outline-none 
-                         bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 
-                         focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 
-                         dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white 
-                         dark:hover:bg-gray-700"
-            >
-              Learn more
-            </a>
+          <div className="flex justify-center items-center">
+          <Image className="rounded-xl"
+                 src={hash}
+                 alt="Hash"
+                 width={250}
+                 height={250}
+                 />
           </div>
         </div>
 
-        <div>
-          <iframe
-            className="mx-auto w-full lg:max-w-xl h-64 rounded-lg sm:h-96 shadow-xl"
-            src=""
-            title="YouTube video player"
-            frameBorder={0}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          />
+        {/* card 2 */}
+        <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-4 md:p-6">
+        <a
+            href="#"
+            className="bg-green-100 text-green-800 text-xl font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-green-400 mb-4"
+          >
+            2.
+          </a>
+          <h2 className="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">
+            What is a Block?
+          </h2>
+          <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">
+            Learn how a block stores a group of verified transactions in the Blockchain.
+          </p>
+          <div className="flex justify-center items-center">
+            <Image
+              className="rounded-xl"
+              src={block}
+              alt="Block"
+              width={200}
+              height={200}
+            />
+          </div>
+        </div>
+
+        {/* card 3 */}
+        <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-4 md:p-6">
+        <a
+            href="#"
+            className="bg-green-100 text-green-800 text-xl font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-green-400 mb-4"
+          >
+            3.
+          </a>
+          <h2 className="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">
+            What is a Blockchain?
+          </h2>
+          <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">
+            Learn how the Blockchain links blocks together to form a secure and trusted record of data.
+          </p>
+          <div className="flex justify-center items-center">
+          <Image className="rounded-xl"
+                 src={blockchain}
+                 alt="Blockchain"
+                 width={500}
+                 height={500}
+                 />
+          </div>
         </div>
       </div>
-    </section>
-  );
-};
-
-export default HomeHeroSection;
+    </div>
+  </section>
+);
