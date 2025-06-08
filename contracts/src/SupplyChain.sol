6c361488d335bc8eb022c31fa94bd53f916254fc
Samuel Parke <samueljparke@gmail.com>
2025-06-08 16:47:34 +0100

    added getShipmentCount back to contract to get shipment id for user

--- contracts/src/SupplyChain.sol
+++ contracts/src/SupplyChain.sol
@@ -37,36 +37,79 @@ contract SupplyChain {
     // events
 
     event ShipmentCreated(
-        address indexed sender, address indexed receiver, uint256 pickupTime, uint256 distance, uint256 price
+        address indexed sender,
+        address indexed receiver,
+        uint256 pickupTime,
+        uint256 distance,
+        uint256 price
     );
 
-    event ShipmentInTransit(address indexed sender, address indexed receiver, uint256 pickupTime);
+    event ShipmentInTransit(
+        address indexed sender,
+        address indexed receiver,
+        uint256 pickupTime
+    );
 
-    event ShipmentDelivered(address indexed sender, address indexed receiver, uint256 deliveryTime);
-    event ShipmentPaid(address indexed sender, address indexed receiver, uint256 amount);
+    event ShipmentDelivered(
+        address indexed sender,
+        address indexed receiver,
+        uint256 deliveryTime
+    );
+    event ShipmentPaid(
+        address indexed sender,
+        address indexed receiver,
+        uint256 amount
+    );
 
-    function createShipment(address _receiver, uint256 _pickupTime, uint256 _distance, uint256 _price) public payable {
+    function createShipment(
+        address _receiver,
+        uint256 _pickupTime,
+        uint256 _distance,
+        uint256 _price
+    ) public payable {
         // checks the actual value being sent (such as 10 ETH) matches the price the user sets
         if (msg.value != _price) {
-            revert PaymentDoesNotMatchPrice(msg.value, _price, "Payment does not match the price user set");
+            revert PaymentDoesNotMatchPrice(
+                msg.value,
+                _price,
+                "Payment does not match the price user set"
+            );
         }
 
         // creates a temporary shipment structure, which then is pushed into mapping(address => Shipment) shipments
         // when creating the shipment: delivery time is 0, shipment status is pending (it was just intialised), and is has not been paid for
-        Shipment memory shipment =
-            Shipment(msg.sender, _receiver, _pickupTime, 0, _distance, _price, ShipmentStatus.PENDING, false);
+        Shipment memory shipment = Shipment(
+            msg.sender,
+            _receiver,
+            _pickupTime,
+            0,
+            _distance,
+            _price,
+            ShipmentStatus.PENDING,
+            false
+        );
 
         // pushes the created shipment struct into main mapping linked to the users address
         shipments[msg.sender].push(shipment);
         // increments the number of shipments created to track the ID
         shipmentCount++;
         // emits an event for the shipment created
-        emit ShipmentCreated(msg.sender, _receiver, _pickupTime, _distance, _price);
+        emit ShipmentCreated(
+            msg.sender,
+            _receiver,
+            _pickupTime,
+            _distance,
+            _price
+        );
     }
 
     // once the shipment is created, it is held within the contract, and the shipping proccess will be started using this function
     // index refers to the ID. The user adds the index when wanting to start the shipment they created
-    function startShipment(address _sender, address _receiver, uint256 _index) public {
+    function startShipment(
+        address _sender,
+        address _receiver,
+        uint256 _index
+    ) public {
         // fetches shipment we created, puts it in storage state as we will change its state on the blockchain
         // we change its status
         // explaination of code: we go into the shipments array, and get the shipment associated with the sender and index(ID) we want.
@@ -76,7 +119,9 @@ contract SupplyChain {
             revert InvalidReceiver();
         }
         if (shipment.status != ShipmentStatus.PENDING) {
-            revert ShipmentNotInIntendedStatus("Shipment is not in Pending state");
+            revert ShipmentNotInIntendedStatus(
+                "Shipment is not in Pending state"
+            );
         }
 
         // changes shipment status to in-transit phase
@@ -87,14 +132,20 @@ contract SupplyChain {
     }
 
     // once in-transit has finished, it is delivered, and we must complete it
-    function completeShipment(address _sender, address _receiver, uint256 _index) public {
+    function completeShipment(
+        address _sender,
+        address _receiver,
+        uint256 _index
+    ) public {
         Shipment storage shipment = shipments[_sender][_index];
 
         if (shipment.receiver != _receiver) {
             revert InvalidReceiver();
         }
         if (shipment.status != ShipmentStatus.IN_TRANSIT) {
-            revert ShipmentNotInIntendedStatus("Shipment is not in In-Transit state");
+            revert ShipmentNotInIntendedStatus(
+                "Shipment is not in In-Transit state"
+            );
         }
         if (shipment.isPaid) {
             revert ShipmentIsAlreadypaid();
@@ -115,10 +166,22 @@ contract SupplyChain {
     }
 
     // get shipment for display
-    function getShipment(address _sender, uint256 _index)
+    function getShipment(
+        address _sender,
+        uint256 _index
+    )
         public
         view
-        returns (address, address, uint256, uint256, uint256, uint256, ShipmentStatus, bool)
+        returns (
+            address,
+            address,
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            ShipmentStatus,
+            bool
+        )
     {
         // we are simply fetching the shipment for display, no state changes
         Shipment memory shipment = shipments[_sender][_index];
@@ -132,5 +195,9 @@ contract SupplyChain {
             shipment.status,
             shipment.isPaid
         );
+    }
+
+    function getShipmentCount(address _sender) public view returns (uint256) {
+        return shipments[_sender].length;
     }
 }
