af1719148f0c1945e35149057610eeed2d2ed143
Samuel Parke <samueljparke@gmail.com>
2025-06-13 08:28:51 +0100

    compliance check functionality added. Unneeded but good to understand its purpose

--- frontend/src/app/rainbowKitConfig.tsx
+++ frontend/src/app/rainbowKitConfig.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { sepolia } from "wagmi/chains";
+import { sepolia, anvil } from "wagmi/chains";
 
 export default getDefaultConfig({
   appName: "Supply chain",
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-  chains: [sepolia],
+  chains: [sepolia, anvil],
   ssr: false,
 });
