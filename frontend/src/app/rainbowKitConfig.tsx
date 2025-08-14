07fa04d7c745868a603b8be40abba37e5b8caf84
Samuel Parke <samueljparke@gmail.com>
2025-08-14 17:09:00 +0100

    recommendation for metamask wallet, or users can use wallets they have installed. This is to not overwhelm students with options

--- frontend/src/app/rainbowKitConfig.tsx
+++ frontend/src/app/rainbowKitConfig.tsx
@@ -2,10 +2,12 @@
 
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { sepolia, anvil } from "wagmi/chains";
+import { metaMaskWallet } from "@rainbow-me/rainbowkit/wallets";
 
 export default getDefaultConfig({
   appName: "Supply chain",
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
   chains: [sepolia, anvil],
+  wallets: [{ groupName: "Recommended", wallets: [metaMaskWallet] }],
   ssr: false,
 });
